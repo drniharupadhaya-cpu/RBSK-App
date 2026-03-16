4dd369a1115195824c86fb768b4fb635e24c7daa
drniharupadhaya-cpu <dr.nihar.upadhaya@gmail.com>
2026-03-16 14:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1939,7 +1939,7 @@ elif menu == "13. Offline Batch Sync":
     # ==========================================
     # MODULE 14: 💻 TECHO PORTAL ENTRY QUEUE
     # ==========================================
-    elif selected_menu == "TECHO Entry Queue":
+elif selected_menu == "TECHO Entry Queue":
         st.header("💻 TECHO Portal Pending Queue")
         st.info("🔒 Safe Mode: View pending children and mark them as entered.")
     
